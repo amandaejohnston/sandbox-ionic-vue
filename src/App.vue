03eb00643c50f701dcf8ce05b5238318b9f83f5e
amandaesmith333 <amandaesmith333@gmail.com>
2021-12-07 13:56:56 -0600

    initial for branch

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,63 @@
 <template>
   <ion-app>
-    <ion-router-outlet />
+    <ion-page>
+      <ion-header>
+        <ion-toolbar>
+          <ion-title>Header</ion-title>
+          <ion-buttons slot="end">
+            <ion-button @click="presentInlineModal()">Inline</ion-button>
+            <ion-button @click="presentControllerModal()">Controller</ion-button>
+          </ion-buttons>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content>Content</ion-content>
+      <ion-modal :is-open="isOpenRef" @didDismiss="setOpen(false)">
+        <Modal></Modal>
+      </ion-modal>
+    </ion-page>
   </ion-app>
 </template>
 
 <script lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import { IonApp, IonPage, IonHeader, IonModal, IonToolbar, IonTitle, IonButtons, IonButton, IonContent, modalController } from '@ionic/vue';
+import { defineComponent, ref } from 'vue';
+import Modal from './components/Modal.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     IonApp,
-    IonRouterOutlet
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonButtons,
+    IonButton,
+    IonContent,
+    IonModal,
+    Modal
+  },
+  setup() {
+    const isOpenRef = ref(false);
+    const setOpen = (state: boolean) => isOpenRef.value = state;
+
+    const presentControllerModal = async () => {
+      const modal = await modalController.create({
+        component: Modal
+      });
+      return modal.present();
+    };
+
+    const presentInlineModal = () => {
+      setOpen(true);
+    };
+
+    return {
+      presentControllerModal,
+      presentInlineModal,
+      setOpen,
+      isOpenRef
+    };
   }
 });
 </script>
